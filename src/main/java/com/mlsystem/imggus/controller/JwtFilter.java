68bc33f0e73e02704faed73f6a01b118f289e0eb
Ewelina Post <ewelina.post@mlsystem.pl>
2022-04-22 15:07:36 +0200

    SecurityUpdate2

--- src/main/java/com/mlsystem/imggus/controller/JwtFilter.java
+++ src/main/java/com/mlsystem/imggus/controller/JwtFilter.java
@@ -3,18 +3,30 @@ package com.mlsystem.imggus.controller;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.Properties;
+import java.util.Set;
 
 public class JwtFilter extends BasicAuthenticationFilter {
+
+
+
     public JwtFilter(AuthenticationManager authenticationManager) {
         super(authenticationManager);
     }
@@ -22,18 +34,25 @@ public class JwtFilter extends BasicAuthenticationFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
             throws IOException, ServletException {
         String header = request.getHeader("Authorization");
-
         UsernamePasswordAuthenticationToken authResult = getAuthenticationByToken(header);
-
         SecurityContextHolder.getContext().setAuthentication(authResult);
+        chain.doFilter(request, response);
     }
 
     private UsernamePasswordAuthenticationToken getAuthenticationByToken(String header) {
-        Jws<Claims> claimsJws = Jwts.parser().setSigningKey("HX*|$!195.6FJET".getBytes())
-                .parseClaimsJws(header.replace("Bearer", ""));
-        return null;
+
+
+        Jws<Claims> claimsJws = Jwts.parser().setSigningKey("aaa".getBytes())
+                .parseClaimsJws(header.replace("Bearer",""));
+
+
+        String username = claimsJws.getBody().get("name").toString();
+        String role = claimsJws.getBody().get("role").toString();
+        Set<SimpleGrantedAuthority> simpleGrantedAuthorities = Collections.singleton(new SimpleGrantedAuthority(role));
+
+        UsernamePasswordAuthenticationToken usernamePasswordAutenthicationToken
+                = new UsernamePasswordAuthenticationToken(username, null, simpleGrantedAuthorities);
+        return usernamePasswordAutenthicationToken;
     }
-
-
 
 }
